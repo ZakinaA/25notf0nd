21a2500f67467ca4cc23904aed40e09f1ffd48b3
clelong14 <colynne.lelong.pro@gmail.com>
2025-10-13 14:49:22 +0200

    Création d'instrument et ajout des relations

--- src/Entity/Couleur.php
+++ src/Entity/Couleur.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CouleurRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CouleurRepository::class)]
@@ -15,6 +17,17 @@ class Couleur
 
     #[ORM\Column(length: 80)]
     private ?string $nom = null;
+
+    /**
+     * @var Collection<int, Instrument>
+     */
+    #[ORM\OneToMany(targetEntity: Instrument::class, mappedBy: 'couleur')]
+    private Collection $instruments;
+
+    public function __construct()
+    {
+        $this->instruments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Couleur
     public function setNom(string $nom): static
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Instrument>
+     */
+    public function getInstruments(): Collection
+    {
+        return $this->instruments;
+    }
+
+    public function addInstrument(Instrument $instrument): static
+    {
+        if (!$this->instruments->contains($instrument)) {
+            $this->instruments->add($instrument);
+            $instrument->setCouleur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeInstrument(Instrument $instrument): static
+    {
+        if ($this->instruments->removeElement($instrument)) {
+            // set the owning side to null (unless already changed)
+            if ($instrument->getCouleur() === $this) {
+                $instrument->setCouleur(null);
+            }
+        }
 
         return $this;
     }
