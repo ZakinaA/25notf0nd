f47ffbe34cb6e12930a67b03f28419170eb4f1c3
clelong14 <colynne.lelong.pro@gmail.com>
2025-10-15 09:19:39 +0200

    Ajout du controlleur pour les instruments

---
+++ src/Form/InstrumentType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Couleur;
+use App\Entity\Instrument;
+use App\Entity\Marque;
+use App\Entity\TypeInstrument;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InstrumentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('numSerie')
+            ->add('dateAchat')
+            ->add('prixAchat')
+            ->add('utilisation')
+            ->add('cheminImage')
+            ->add('typeInstrument', EntityType::class, [
+                'class' => TypeInstrument::class,
+                'choice_label' => 'id',
+            ])
+            ->add('couleur', EntityType::class, [
+                'class' => Couleur::class,
+                'choice_label' => 'id',
+            ])
+            ->add('marque', EntityType::class, [
+                'class' => Marque::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Instrument::class,
+        ]);
+    }
+}
