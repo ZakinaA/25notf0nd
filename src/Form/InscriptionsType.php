61703554064e1a2eaba376d060f8f6af19ce0807
LiamDuval <liamd4047@gmail.com>
2025-12-01 16:24:09 +0100

    inscription_cours2

--- src/Form/InscriptionsType.php
+++ src/Form/InscriptionsType.php
@@ -7,7 +7,6 @@ use App\Entity\Eleves;
 use App\Entity\Inscriptions;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,56 +15,23 @@ class InscriptionsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nombre_paiments', IntegerType::class, [
-                'label' => 'Nombre de paiements',
-                'data' => 1,
-                'attr' => [
-                    'min' => 1,
-                    'max' => 12
-                ]
-            ]);
-
-        // Si des élèves sont passés en option (pour les adhérents)
-        if (!empty($options['eleves'])) {
-            $builder->add('eleves', EntityType::class, [
+            ->add('nombre_paiments')
+            ->add('dateInscription')
+            ->add('eleves', EntityType::class, [
                 'class' => Eleves::class,
-                'choices' => $options['eleves'],
-                'choice_label' => function(Eleves $eleve) {
-                    return $eleve->getPrenom() . ' ' . $eleve->getNom();
-                },
-                'label' => 'Sélectionner un élève',
-                'placeholder' => 'Choisir un élève'
-            ]);
-        } else {
-            // Pour les admins : tous les élèves
-            $builder->add('eleves', EntityType::class, [
-                'class' => Eleves::class,
-                'choice_label' => function(Eleves $eleve) {
-                    return $eleve->getPrenom() . ' ' . $eleve->getNom();
-                },
-                'label' => 'Élève',
-                'placeholder' => 'Choisir un élève'
-            ]);
-        }
-
-        // Si un cours n'est pas déjà défini (pour les admins)
-        if (empty($options['data']->getCours())) {
-            $builder->add('cours', EntityType::class, [
+                'choice_label' => 'id',
+            ])
+            ->add('cours', EntityType::class, [
                 'class' => Cours::class,
-                'choice_label' => function(Cours $cours) {
-                    return $cours->getLibelle() . ' - ' . $cours->getProfesseur()->getNom();
-                },
-                'label' => 'Cours',
-                'placeholder' => 'Choisir un cours'
-            ]);
-        }
+                'choice_label' => 'id',
+            ])
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Inscriptions::class,
-            'eleves' => []
         ]);
     }
 }
